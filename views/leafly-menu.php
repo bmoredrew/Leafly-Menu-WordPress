02c8af59640b37688957fffdb9b6e3ccaf04cbab
ted <tedgeving@users.noreply.github.com>
2016-09-15 10:02:43 -0600

    Updated Embed Code
    New and improved embed code fully responsive.

--- views/leafly-menu.php
+++ views/leafly-menu.php
@@ -1,1 +1,3 @@
-<div id="leafly-menu"><iframe src="http://www.leafly.com/embed/menu/<?php echo $content ?>?initialWidth=1029&amp;childId=leafly-menu" width="100%" scrolling="no" marginheight="0" frameborder="0" height="6347px"></iframe></div>
+<div id="leafly-menu"></div>
+<script src="https://www.leafly.com/public/global/js/dispensarymanager/embed.js"></script>
+<script>var pymParent = pym.Parent('leafly-menu', 'https://www.leafly.com/embed/menu2/<?php echo $content ?>', {});</script>
